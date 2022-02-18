6a034ebc9fa29bd3555389d23cc42a04e2d17122
Enzo-Campos <93542684+Enzo-Campos@users.noreply.github.com>
2022-02-18 17:45:38 -0300

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,77 @@
+import './app.css'
+import React from "react";
+import tmdb from "./tmdb.js";
+import { useEffect } from "react";
+import { useState } from "react";
+import MovieRow from "./components/movieRow";
+import FeaturedMovie from './components/featuredMovie';
+import Header from "./components/header"
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null)
+  const [blackHeader, setBlackHeader] = useState(false)
+
+  useEffect(() =>{
+    const loadAll = async () => {
+      //Pegando a lista inteira
+      let list = await tmdb.getHomeList();
+      setMovieList(list)
+
+      //Pegando o fetured
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll()
+  }, []);
+
+  useEffect(() =>{
+    const scrollListener = () =>{
+      if(window.scrollY > 10){
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return() => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+
+  }, [])
+
+
+  return (
+    <div className="page">
+
+      <Header black={blackHeader} />
+
+    {featuredData &&
+    <FeaturedMovie item={featuredData}/>}
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items}/>
+        ))}
+      </section>
+
+      <footer>
+        Feito com <span role='img' aria-label='coração'>💖</span> por Enzo Campos<br/>
+        Direitos de imagem para Netflix<br/>
+        Dados pegos da API de Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 &&
+      <div className='loading'>
+         <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"/>
+      </div>}
+    </div>
+  )
+}
---
+++ my-app/src/components/movieRow/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import './MovieRow.css';
+import { useState } from "react";
+
+export default ({title, items}) => {
+    const [scrollX, setScrollX] = useState(-500)
+    
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0){
+            x = 0
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow  = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 170;
+        if ((window.innerWidth - listW) > x ){
+            x = (window.innerWidth - listW) - 60
+        }
+        setScrollX(x)
+    }
+
+
+    return (
+        <div className="movieRow">
+             <h2>{title}</h2>
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path  fill="white" d="M30.83 14.83l-2.83-2.83-12 12 12 12 2.83-2.83-9.17-9.17z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>
+            </div>
+            <div className="movieRow--right" onClick={handleRightArrow}>
+                <svg height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path fill="white" d="M20 12l-2.83 2.83 9.17 9.17-9.17 9.17 2.83 2.83 12-12z"/><path d="M0 0h48v48h-48z" fill="none"/></svg>
+            </div>
+            <div className="movieRow--listarea">
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 200
+                }}>
+                    {items.results.length > 0 && items.results.map((item, key) => (
+                        <div className="movieRow--item" key={key}>
+                            <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+}
